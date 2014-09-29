8bbc28028634e82f8fb04fc77f76e3f3573b46c1
Damien Pontifex <damien.pontifex@gmail.com>
2014-09-29 10:56:07 +0800

    Added chapter one implementation

---
+++ Ch1/MatVec/MatVec/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  MatVec
+//
+//  Created by Damien Pontifex on 28/09/2014.
+//  Copyright (c) 2014 Damien Pontifex. All rights reserved.
+//
+
+#ifndef MatVec_Bridging_Header_h
+#define MatVec_Bridging_Header_h
+
+#import "matvec.cl.h"
+
+#endif
---
+++ Ch1/MatVec/MatVec/main.swift
@@ -0,0 +1,69 @@
+//
+//  main.swift
+//  MatVec
+//
+//  Created by Damien Pontifex on 28/09/2014.
+//  Copyright (c) 2014 Damien Pontifex. All rights reserved.
+//
+
+import Foundation
+import OpenCL
+
+var mat = [cl_float](count: 16, repeatedValue: 0.0)
+var vec = [cl_float](count: 4, repeatedValue: 0.0)
+var result = [cl_float](count: 4, repeatedValue: 0.0)
+var correct = [cl_float](count: 4, repeatedValue: 0.0)
+
+/* Initialize data to be processed by the kernel */
+for i in 0..<16 {
+	mat[i] = Float(i) * 2.0;
+}
+for i in 0..<4 {
+	vec[i] = Float(i) * 3.0;
+	correct[0] += mat[i]    * vec[i];
+	correct[1] += mat[i+4]  * vec[i];
+	correct[2] += mat[i+8]  * vec[i];
+	correct[3] += mat[i+12] * vec[i];
+}
+
+let context = gcl_get_context()
+let queue = gcl_create_dispatch_queue(cl_queue_flags(CL_DEVICE_TYPE_GPU), nil)
+
+dispatch_sync(queue) {
+
+	var ndRange = cl_ndrange(
+		work_dim: 1,
+		global_work_offset: (0, 0, 0),
+		global_work_size: (4, 0, 0),
+		local_work_size: (0, 0, 0)
+	)
+	
+	var mat_buff = gcl_malloc(UInt(sizeof(cl_float) * 16), &mat, cl_malloc_flags(CL_MEM_COPY_HOST_PTR | CL_MEM_READ_ONLY))
+	var vec_buff = gcl_malloc(UInt(sizeof(cl_float) * 4), &vec, cl_malloc_flags(CL_MEM_COPY_HOST_PTR | CL_MEM_READ_ONLY))
+	var res_buff = gcl_malloc(UInt(sizeof(cl_float) * 4), nil, cl_malloc_flags(CL_MEM_WRITE_ONLY))
+	
+	var resC = UnsafeMutablePointer<cl_float>(COpaquePointer(res_buff))
+	var matPointer = COpaquePointer(mat_buff)
+	var vecPointer = COpaquePointer(vec_buff)
+	
+	var ndRangePointer = withUnsafePointer(&ndRange, { (unsafePointer) -> UnsafePointer<cl_ndrange> in
+		return unsafePointer
+	})
+
+	matvec_mult_kernel(ndRangePointer, matPointer, vecPointer, resC)
+	
+	gcl_memcpy(&result, res_buff, UInt(sizeof(cl_float) * 4))
+	
+	/* Test the result */
+	if((result[0] == correct[0]) && (result[1] == correct[1])
+		&& (result[2] == correct[2]) && (result[3] == correct[3])) {
+			println("Matrix-vector multiplication successful")
+	}
+	else {
+		println("Matrix-vector multiplication unsuccessful")
+	}
+
+	gcl_free(mat_buff)
+	gcl_free(vec_buff)
+	gcl_free(res_buff)
+}
